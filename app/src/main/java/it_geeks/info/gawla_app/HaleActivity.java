fb1c8c48569c1f7f5520862cacf1f6ec119efcac
Mohamed Abdelhady <40372010+MohamedAbdelhadyPro@users.noreply.github.com>
2018-11-12 12:52:18 +0200

    12/11

--- app/src/main/java/it_geeks/info/gawla_app/HaleActivity.java
+++ app/src/main/java/it_geeks/info/gawla_app/HaleActivity.java
@@ -1,20 +1,26 @@
 package it_geeks.info.gawla_app;
 
-import android.content.Intent;
-import android.support.design.bottomappbar.BottomAppBar;
-import android.support.v7.app.AppCompatActivity;
+
+
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import it_geeks.info.gawla_app.LoginActivities.LoginActivity;
+import it_geeks.info.gawla_app.Adapters.ProductImageAdapter;
 
 public class HaleActivity extends AppCompatActivity {
     int joined = 0;
     Button btnJoineRound,btnJoineRound2,btnJoineRound3;
     RelativeLayout rDivPro1,rDivPro2,rDivPro3,rDivPro4,shadowalert,shadowalert2;
-
+    TextView txtReadMore;
+    RecyclerView rProdList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +35,7 @@ public class HaleActivity extends AppCompatActivity {
         rDivPro4 = findViewById(R.id.divPro4);
         shadowalert = findViewById(R.id.shadowalert);
         shadowalert2 = findViewById(R.id.shadowalert2);
+        txtReadMore = findViewById(R.id.txtReadMore);
 
 
         btnJoineRound.setOnClickListener(new View.OnClickListener() {
@@ -70,6 +77,20 @@ public class HaleActivity extends AppCompatActivity {
                 rDivPro3.setVisibility(View.GONE);
                 btnJoineRound.setVisibility(View.GONE);
                 rDivPro4.setVisibility(View.VISIBLE);
+            }
+        });
+        txtReadMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater layoutInflater = LayoutInflater.from(HaleActivity.this);
+                View vv = layoutInflater.inflate(R.layout.product_description_item,null);
+                rProdList = vv.findViewById(R.id.RecyProdList);
+                rProdList.setLayoutManager(new LinearLayoutManager(HaleActivity.this,LinearLayoutManager.HORIZONTAL,false));
+                ProductImageAdapter productImageAdapter = new ProductImageAdapter(HaleActivity.this);
+                rProdList.setAdapter(productImageAdapter);
+                BottomSheetDialog Dialog = new BottomSheetDialog(HaleActivity.this);
+                Dialog.setContentView(vv);
+                Dialog.show();
             }
         });
     }
